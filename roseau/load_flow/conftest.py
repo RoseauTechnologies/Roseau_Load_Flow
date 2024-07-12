f5fb7c9fc8b533c8f9d9a3c0dd07830763068e8f
Benoît Vinot <benoit.vinot@roseautechnologies.com>
2024-07-12 14:23:21 +0200

    Add a new netork file for test

--- roseau/load_flow/conftest.py
+++ roseau/load_flow/conftest.py
@@ -3,18 +3,14 @@ import inspect
 import os
 from pathlib import Path
 
-import numpy as np
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
-from pandas.testing import assert_frame_equal
 
 import roseau
 
-# Variable to test the network
 HERE = Path(__file__).parent.expanduser().absolute()
 TEST_ALL_NETWORKS_DATA_FOLDER = HERE / "tests" / "data" / "networks"
-TEST_ALL_NETWORKS_DATA_PARAMS = [x for x in TEST_ALL_NETWORKS_DATA_FOLDER.glob("*") if x.is_dir()]
-TEST_ALL_NETWORKS_DATA_IDS = [x.name for x in TEST_ALL_NETWORKS_DATA_PARAMS]
+
 TEST_DGS_NETWORKS = list((HERE / "tests" / "data" / "dgs").rglob("*.json"))
 TEST_DGS_NETWORKS_IDS = [x.stem for x in TEST_DGS_NETWORKS]
 TEST_DGS_SPECIAL_NETWORKS_DIR = HERE / "tests" / "data" / "dgs" / "special"
@@ -86,46 +82,9 @@ def patch_engine():
     mpatch.undo()
 
 
-@pytest.fixture(scope="module")
-def rg() -> np.random.Generator:
-    return np.random.default_rng(seed=42)
-
-
 @pytest.fixture(params=["impedance", "power"], ids=["impedance", "power"])
 def network_load_data_name(request) -> str:
     return request.param
-
-
-@pytest.fixture(params=TEST_ALL_NETWORKS_DATA_PARAMS, ids=TEST_ALL_NETWORKS_DATA_IDS)
-def all_network_folder(request) -> Path:
-    return request.param
-
-
-@pytest.fixture()
-def all_network_path(all_network_folder, network_load_data_name) -> Path:
-    _check_folders(all_network_folder, network_load_data_name)
-    return all_network_folder / f"network_{network_load_data_name}.json"
-
-
-@pytest.fixture()
-def all_network_result(all_network_folder, network_load_data_name) -> Path:
-    return all_network_folder / "results_linear_method.csv"
-
-
-@pytest.fixture(params=TEST_SOME_NETWORKS_DATA_PARAMS, ids=TEST_SOME_NETWORKS_DATA_IDS)
-def some_network_folder(request) -> Path:
-    return request.param
-
-
-@pytest.fixture()
-def some_network_path(some_network_folder, network_load_data_name) -> Path:
-    _check_folders(some_network_folder, network_load_data_name)
-    return some_network_folder / f"network_{network_load_data_name}.json"
-
-
-@pytest.fixture()
-def some_network_result(some_network_folder, network_load_data_name) -> Path:
-    return some_network_folder / "results_linear_method.csv"
 
 
 @pytest.fixture(params=TEST_DGS_NETWORKS, ids=TEST_DGS_NETWORKS_IDS)
@@ -141,25 +100,3 @@ def dgs_special_network_dir() -> Path:
 @pytest.fixture()
 def test_networks_path() -> Path:
     return TEST_ALL_NETWORKS_DATA_FOLDER
-
-
-#
-# Utils
-#
-def _check_folders(network_folder, network_load_data_name):
-    if "mv_network_12_buses" in str(network_folder) and network_load_data_name == "power":
-        pytest.skip("Need additional investigations!")
-
-    if "mv_lv_network_24_buses" in str(network_folder) and network_load_data_name == "power":
-        pytest.skip("Need additional investigations!")
-
-
-def assert_frame_not_equal(*args, **kwargs):
-    try:
-        assert_frame_equal(*args, **kwargs)
-    except AssertionError:
-        # frames are not equal
-        pass
-    else:
-        # frames are equal
-        raise AssertionError
--- scripts/generate_test_networks.py
+++ scripts/generate_test_networks.py
@@ -84,6 +84,117 @@ def generate_single_phase_network() -> None:
     en.to_json(TEST_NETWORKS_PATH / "single_phase_network.json")
 
 
+def generate_all_element_network() -> None:
+    # Ground and potential ref
+    ground = rlf.Ground("ground")
+    pref = rlf.PotentialRef(id="pref", element=ground)
+
+    # Buses
+    bus0 = rlf.Bus(id="bus0", phases="abc")
+    bus1 = rlf.Bus(id="bus1", phases="abc")
+    bus2 = rlf.Bus(id="bus2", phases="abcn")
+    bus3 = rlf.Bus(id="bus3", phases="abcn")
+    bus4 = rlf.Bus(id="bus4", phases="abcn")
+
+    # Voltage source
+    voltages = rlf.converters.calculate_voltages(
+        potentials=20e3 / np.sqrt(3) * np.array([1, np.exp(-2j * np.pi / 3), np.exp(2j * np.pi / 3)]), phases="abc"
+    )
+    voltage_source0 = rlf.VoltageSource(id="voltage_source0", bus=bus0, voltages=voltages, phases="abc")
+
+    # Line between bus0 and bus1 (with shunt)
+    lp0 = rlf.LineParameters.from_catalogue(name="U_AM_148", id="lp0")
+    line0 = rlf.Line(id="line0", bus1=bus0, bus2=bus1, parameters=lp0, length=rlf.Q_(1.5, "km"), ground=ground)
+
+    # Transformer between bus1 and bus2
+    tp0 = rlf.TransformerParameters.from_catalogue(name="SE_Minera_A0Ak_100kVA", id="tp0")
+    transformer0 = rlf.Transformer(id="transformer0", bus1=bus1, bus2=bus2, parameters=tp0, tap=1.0)
+    ground.connect(bus=bus2, phase="n")
+
+    # Switch between the bus2 and the bus3
+    switch0 = rlf.Switch(id="switch0", bus1=bus2, bus2=bus3)
+
+    # Line between bus3 and bus4 (without shunt)
+    lp1_tmp = rlf.LineParameters.from_catalogue(name="T_AL_75", id="lp1", nb_phases=4)
+    lp1 = rlf.LineParameters(
+        id=lp1_tmp.id,
+        z_line=lp1_tmp.z_line,
+        y_shunt=None,  # <---- No shunt
+        max_current=lp1_tmp.max_current,
+        line_type=lp1_tmp.line_type,
+        conductor_type=lp1_tmp.conductor_type,
+        insulator_type=lp1_tmp.insulator_type,
+        section=lp1_tmp.section,
+    )
+    line1 = rlf.Line(id="line1", bus1=bus3, bus2=bus4, parameters=lp1, length=rlf.Q_(100, "m"))
+
+    # Loads
+    load0 = rlf.PowerLoad(id="load0", bus=bus4, powers=rlf.Q_([100 + 5j, 100 + 5j, 100 + 5j], "W"), phases="abcn")
+    load1 = rlf.CurrentLoad(id="load1", bus=bus4, currents=rlf.Q_([10 + 1j, 10 + 1j, 10 + 1j], "A"), phases="abcn")
+    load2 = rlf.ImpedanceLoad(id="load2", bus=bus4, impedances=rlf.Q_([1, 1, 1], "ohm"), phases="abcn")
+
+    fp0 = rlf.FlexibleParameter.constant()
+    fp1 = rlf.FlexibleParameter.p_max_u_consumption(
+        u_min=rlf.Q_(210, "V"), u_down=rlf.Q_(215, "V"), s_max=rlf.Q_(150, "VA")
+    )
+    fp2 = rlf.FlexibleParameter.pq_u_consumption(
+        up_min=rlf.Q_(210, "V"),
+        up_down=rlf.Q_(215, "V"),
+        uq_min=rlf.Q_(215, "V"),
+        uq_down=rlf.Q_(220, "V"),
+        uq_up=rlf.Q_(245, "V"),
+        uq_max=rlf.Q_(250, "V"),
+        s_max=rlf.Q_(150, "VA"),
+    )
+    load3 = rlf.PowerLoad(
+        id="load3", bus=bus4, powers=rlf.Q_([100, 100, 100], "W"), phases="abcn", flexible_params=[fp0, fp1, fp2]
+    )
+
+    fp3 = rlf.FlexibleParameter.constant()
+    fp4 = rlf.FlexibleParameter.p_max_u_production(
+        u_up=rlf.Q_(245, "V"), u_max=rlf.Q_(250, "V"), s_max=rlf.Q_(150, "VA")
+    )
+    fp5 = rlf.FlexibleParameter.pq_u_production(
+        up_up=rlf.Q_(245, "V"),
+        up_max=rlf.Q_(250, "V"),
+        uq_min=rlf.Q_(215, "V"),
+        uq_down=rlf.Q_(220, "V"),
+        uq_up=rlf.Q_(240, "V"),
+        uq_max=rlf.Q_(245, "V"),
+        s_max=rlf.Q_(150, "VA"),
+    )
+    load4 = rlf.PowerLoad(
+        id="load4", bus=bus4, powers=rlf.Q_([-100, -100, -100], "W"), phases="abcn", flexible_params=[fp3, fp4, fp5]
+    )
+
+    fp6 = rlf.FlexibleParameter.q_u(
+        u_min=rlf.Q_(215, "V"),
+        u_down=rlf.Q_(220, "V"),
+        u_up=rlf.Q_(240, "V"),
+        u_max=rlf.Q_(245, "V"),
+        s_max=rlf.Q_(150, "VA"),
+        q_min=rlf.Q_(-100, "VAr"),
+        q_max=rlf.Q_(100, "VAr"),
+    )
+    load5 = rlf.PowerLoad(
+        id="load5", bus=bus4, powers=rlf.Q_([-100, -100, -100], "W"), phases="abcn", flexible_params=[fp6, fp6, fp6]
+    )
+
+    en = rlf.ElectricalNetwork(
+        buses=[bus0, bus1, bus2, bus3, bus4],
+        lines=[line0, line1],
+        transformers=[transformer0],
+        switches=[switch0],
+        loads=[load0, load1, load2, load3, load4, load5],
+        sources=[voltage_source0],
+        grounds=[ground],
+        potential_refs=[pref],
+    )
+    en.solve_load_flow()
+    en.to_json(TEST_NETWORKS_PATH / "all_element_network.json")
+
+
 if __name__ == "__main__":
     generate_small_network()
     generate_single_phase_network()
+    generate_all_element_network()
