f8d0a8039865d8a602c4d4e5e1bf0129f42ee0f8
Ali Hamdan <ali.hamdan@roseautechnologies.com>
2024-11-29 14:22:22 +0100

    Fix transformer parameters setter

--- roseau/load_flow/models/transformers/transformers.py
+++ roseau/load_flow/models/transformers/transformers.py
@@ -168,6 +168,8 @@ class Transformer(AbstractBranch):
         self._invalidate_network_results()
         if self._cy_element is not None:
             z2, ym, k = value._z2, value._ym, value._k
+            if value.type in ("single-phase", "center-tapped"):
+                k *= value._orientation
             self._cy_element.update_transformer_parameters(z2, ym, k * self.tap)
 
     @property
