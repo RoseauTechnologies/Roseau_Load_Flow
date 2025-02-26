14035902c8767faae9546fde98bd67e53b461b2d
Ali Hamdan <ali.hamdan@roseautechnologies.com>
2025-02-26 12:09:48 +0100

    scope session

--- roseau/load_flow/conftest.py
+++ roseau/load_flow/conftest.py
@@ -137,16 +137,16 @@ def dgs_network_path(request) -> Path:
     return request.param
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def dgs_networks_dir() -> Path:
     return TEST_DGS_NETWORKS_DIR
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def dgs_special_networks_dir() -> Path:
     return TEST_DGS_SPECIAL_NETWORKS_DIR
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def test_networks_path() -> Path:
     return TEST_ALL_NETWORKS_DATA_FOLDER
