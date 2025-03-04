4df73f796bd5d846fbcefe9a0d66c376ab95008f
Ali Hamdan <ali.hamdan@roseautechnologies.com>
2025-03-04 14:39:18 +0100

    Remove the transformer specific results (#345)
    These results were added recently so I am removing them before they go
    into a release. A more generic and simpler solution will be implemented
    later as proposed in #344

--- roseau/load_flow/models/tests/test_transformers.py
+++ roseau/load_flow/models/tests/test_transformers.py
@@ -109,18 +109,6 @@ def test_transformer_results():
     assert np.isscalar(actual_total_losses)
     np.testing.assert_allclose(actual_total_losses, expected_total_losses)
 
-    # Transformer HV aliases
-    np.testing.assert_allclose(tr.res_potentials_hv.m, tr.res_potentials[0].m)
-    np.testing.assert_allclose(tr.res_currents_hv.m, tr.res_currents[0].m)
-    np.testing.assert_allclose(tr.res_voltages_hv.m, tr.res_voltages[0].m)
-    np.testing.assert_allclose(tr.res_powers_hv.m, tr.res_powers[0].m)
-
-    # Transformer LV aliases
-    np.testing.assert_allclose(tr.res_potentials_lv.m, tr.res_potentials[1].m)
-    np.testing.assert_allclose(tr.res_currents_lv.m, tr.res_currents[1].m)
-    np.testing.assert_allclose(tr.res_voltages_lv.m, tr.res_voltages[1].m)
-    np.testing.assert_allclose(tr.res_powers_lv.m, tr.res_powers[1].m)
-
 
 def test_brought_out_neutral():
     bus_hv = Bus(id="bus_hv", phases="abcn")
