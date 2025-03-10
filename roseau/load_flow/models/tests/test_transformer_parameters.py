Fix invalid transformer params with no leakage reactance (#350)
Fixes #349

roseau/load_flow/models/tests/test_transformer_parameters.py
@@ -1,6 +1,7 @@
 import numbers
 
 import numpy as np
+import numpy.testing as npt
 import pandas as pd
 import pytest
 from pint import DimensionalityError
@@ -780,19 +781,6 @@ def test_equality():
     assert tp != object()
 
 
-@pytest.mark.xfail(reason="CyTr signature changed (TODO remove xfail)")
-@pytest.mark.no_patch_engine
-def test_compute_open_short_circuit_parameters():
-    tp = TransformerParameters.from_catalogue(name="SE Minera A0Ak 100kVA 15/20kV(20) 410V Dyn11")
-    p0, i0 = tp._compute_open_circuit_parameters()
-    assert np.isclose(p0.m, tp.p0.m)
-    assert np.isclose(i0.m, tp.i0.m)
-
-    psc, vsc = tp._compute_short_circuit_parameters()
-    assert np.isclose(psc.m, tp.psc.m, rtol=0.001)
-    assert np.isclose(vsc.m, tp.vsc.m)
-
-
 def test_ideal_transformer():
     # Ideal transformer not yet supported
     with pytest.raises(RoseauLoadFlowException) as e:
@@ -803,3 +791,22 @@ def test_ideal_transformer():
     assert e.value.code == RoseauLoadFlowExceptionCode.BAD_TRANSFORMER_IMPEDANCE
     # OK
     TransformerParameters(id="test", vg="Dyn11", sn=50e3, uhv=20e3, ulv=400, z2=0.0000001, ym=0.0)
+
+
+def test_from_tests_no_leakage_reactance():
+    # https://github.com/RoseauTechnologies/Roseau_Load_Flow/issues/349
+    # Data from LV isolation transformer "DYN11 K13 CU IP21 8" of "ORTEA NEXT"
+    # https://www.orteanext.com/product/dyn11-k13-r-ip21/
+    tp = TransformerParameters.from_open_and_short_circuit_tests(
+        id="DYN11 K13 CU IP21 8",
+        vg="Dyn11",
+        uhv=Q_(400, "V"),
+        ulv=Q_(400, "V"),
+        sn=Q_(8, "kVA"),
+        p0=Q_(150, "W"),
+        i0=Q_(10, "percent"),
+        psc=Q_(280, "W"),
+        vsc=Q_(3.5, "percent"),
+    )
+    npt.assert_allclose(tp.z2.m.real, tp.vsc.m * tp.ulv.m**2 / tp.sn.m)  # r2 = |z2|
+    npt.assert_allclose(tp.z2.m.imag, 0.0)  # x2 = 0

roseau/load_flow/models/transformers/parameters.py
@@ -374,7 +374,8 @@ class TransformerParameters(Identifiable, JsonMixin, CatalogueMixin[pd.DataFrame
     ) -> tuple[complex, complex]:
         whv, wlv, _ = cls.extract_windings(vg=vg)
 
-        # Off-load test
+        # Open-circuit (no-load) test
+        # ---------------------------
         # Iron losses resistance (Ohm)
         if p0 > 0:
             r_iron = uhv**2 / p0
@@ -389,14 +390,20 @@ class TransformerParameters(Identifiable, JsonMixin, CatalogueMixin[pd.DataFrame
         else:  # no magnetizing reactance
             y_lm = 0j
         ym = y_iron + y_lm
-
-        # Short-circuit test
-        r2 = psc * (ulv / sn) ** 2
-        l2_omega = np.sqrt((vsc * ulv**2 / sn) ** 2 - r2**2)
-        z2 = r2 + 1j * l2_omega
-
         if whv[0] == "D":
             ym /= 3
+
+        # Short-circuit test
+        # ------------------
+        # Copper losses resistance (Ohm)
+        r2 = psc * (ulv / sn) ** 2
+        z2_mag = vsc * ulv**2 / sn
+        # Leakage inductance (Henry) * omega (rad/s)
+        if z2_mag > r2:  # noqa: SIM108
+            l2_omega = np.sqrt(z2_mag**2 - r2**2)
+        else:  # no leakage reactance
+            l2_omega = 0.0
+        z2 = r2 + 1j * l2_omega
         if wlv[0] == "d":
             z2 *= 3
 
@@ -1139,13 +1146,13 @@ class TransformerParameters(Identifiable, JsonMixin, CatalogueMixin[pd.DataFrame
                 The rated no-load phase-to-phase voltage of the LV side of the transformer to get.
 
             id:
-                A unique ID for the created line parameters object (optional). If ``None``
+                A unique ID for the created transformer parameters object (optional). If ``None``
                 (default), the id of the created object will be its name in the catalogue. Note that
                 this parameter is not used in the data filtering.
 
         Returns:
-            The selected transformer. If several transformers fitting the filters are in the catalogue, an error is
-            raised.
+            The selected transformer. If several or no transformers fitting the filters are found in
+            the catalogue, an error is raised.
         """
         # Get the catalogue data
         catalogue_data, query_info = cls._get_catalogue(