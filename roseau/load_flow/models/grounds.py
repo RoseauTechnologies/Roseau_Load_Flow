68dc6623cf096bda62a35c5b56d87f8884ed0f9a
Ali Hamdan <ali.hamdan@roseautechnologies.com>
2025-04-28 12:11:03 +0200

    Simplify ground connection results

--- roseau/load_flow/models/grounds.py
+++ roseau/load_flow/models/grounds.py
@@ -331,7 +331,7 @@ class GroundConnection(Element[CySimplifiedLine | CySwitch]):
     #
     def _refresh_results(self) -> None:
         if self._fetch_results:
-            self._res_current = self._cy_element.get_side_currents(n1=1, n2=1)[0].item()
+            self._res_current = self._cy_element.get_currents(n=1).item()
 
     def _res_current_getter(self, warning: bool) -> complex:
         self._refresh_results()
